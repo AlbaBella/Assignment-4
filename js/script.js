6d4b38c2feb6b403eb5c7599bb38b2900d274dca
Alba <alba.bella@cix.csi.cuny.edu>
2020-06-03 00:55:30 -0400

    Question 3

---
+++ js/script.js
@@ -0,0 +1,14 @@
+document.querySelector('form').addEventListener('submit', function (e){
+    e.preventDefault();
+    let p1 = document.getElementById('password').value;
+    let username = document.getElementById('username');
+    let tagofH1 = document.getElementById("form");
+    if( p1 === '12345678' && username != "") {
+        tagofH1.innerText = "Filled out!";   
+    }
+    else {
+        alert("Wrong!");
+    }
+    
+    
+});
